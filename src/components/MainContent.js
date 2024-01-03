275e6fb1d4fcceaa0aaf54c293f24b164af83504
JAY2415 <137811465+JAY2415@users.noreply.github.com>
2024-01-03 10:30:48 +0530

    Update MainContent.js

--- src/components/MainContent.js
+++ src/components/MainContent.js
@@ -3,12 +3,12 @@ import image from "../images/group.png"
 
 export default function MainContent() {
 	return (
-		<div className="main--container">
+		<section className="main--container">
 			<img src={image} className="main--img"/>
 			<h1 className="main--h1">Online Experiences</h1>
 			<p className="main--p">
-				Join unique interactive activities led by one-of-a-kind hosts—all without leaving home.
+				Join unique interactive activities led by one-of-a-kind hostsâ€”all without leaving home.
 			</p>
-		</div>
+		</section>
 	)
 }
