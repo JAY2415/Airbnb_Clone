d5a9000b934efb33b86aca32bcc1954cb94fb861
JAY2415 <137811465+JAY2415@users.noreply.github.com>
2024-01-04 22:25:39 +0530

    Update Card.js

--- src/components/Card.js
+++ src/components/Card.js
@@ -5,7 +5,7 @@ import star from "../images/star.png"
 export default function Card(props) {
 	return (
 		<div className="card">
-			<img src={require("../src/images/${props.img}.png")} className="card--img" />
+			<img src={require(`../src/images/${props.img}.png`)} className="card--img" />
 			<div className="card--stats">
 				<img src={star} className="card--star"/>
 				<span>{props.rating}</span>
@@ -13,7 +13,7 @@ export default function Card(props) {
 				<span className="gray">{props.country}</span>
 			</div>
 			<p>{props.title}</p>
-			<p><span className="bold">From ${props.price}</span> / person</p>
+			<p><span className="bold">From $ {props.price}</span> / person</p>
 		</div>
 	)
 }
