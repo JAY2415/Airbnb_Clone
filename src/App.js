ae2aef869339f2ac59e9b43bf75147e22fbfd4ee
JAY2415 <begin.cp.234@gmail.com>
2024-01-05 22:21:34 +0530

    Updates

--- src/App.js
+++ src/App.js
@@ -1,29 +1,32 @@
 import React from "react"
-
-
 import NavBar from "./components/NavBar"
-
 import MainContent from "./components/MainContent"
-
 import Card from "./components/Card"
-
+import data from "./data"
 
 
 export default function App() {
+
+	const cards = data.map(item => {
+		return (
+			<Card      
+        		img={item.coverImg}
+				rating={item.stats.rating}      
+				reviewCount={item.stats.reviewCount}     
+				country={item.location}      
+				title={item.title}
+        		price={item.price}
+			/>
+		)
+	})
+
 
 	return (
 
 		<div>
 			<NavBar />
 			<MainContent />
-			<Card      
-				img="image_12"      
-				rating="5.0"      
-				reviewCount={6}     
-				country="USA"      
-				title="Life Lessons with Katie Zaferes"
-        price={136}
-			/>
+			{cards}
 		</div>
 
 	)
