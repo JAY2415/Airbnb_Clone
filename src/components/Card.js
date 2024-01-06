05f14dd02fca73b5807041f471731ef2a1414fa1
JAY2415 <begin.cp.234@gmail.com>
2024-01-06 22:05:59 +0530

    Resolving Code.

--- src/components/Card.js
+++ src/components/Card.js
@@ -4,9 +4,9 @@ import star from "../images/star.png"
 export default function Card(props) {
 
 	let badgeText
-    if (props.openSpots === 0) {
+    if (props.item.item.openSpots === 0) {
         badgeText = "SOLD OUT"
-    } else if (props.location === "Online") {
+    } else if (props.item.location === "Online") {
         badgeText = "ONLINE"
     }
 
@@ -14,15 +14,15 @@ export default function Card(props) {
 		<div className="card">
 			{badgeText && <div className="card--badge">{badgeText}</div>}
 			
-			<img src={require(`../images/${props.img}.png`)} className="card--img" />
+			<img src={require(`../images/${props.item.coverImg}.png`)} className="card--img" />
 			<div className="card--stats">
 				<img src={star} className="card--star"/>
-				<span>{props.rating}</span>
-				<span className="gray">({props.reviewCount})</span>
-				<span className="gray">{props.country}</span>
+				<span>{props.item.stats.rating}</span>
+				<span className="gray">({props.item.stats.reviewCount})</span>
+				<span className="gray">{props.item.location}</span>
 			</div>
-			<p>{props.title}</p>
-			<p><span className="bold">From $ {props.price}</span> / person</p>
+			<p>{props.item.title}</p>
+			<p><span className="bold">From $ {props.item.price}</span> / person</p>
 		</div>
 	)
 }
